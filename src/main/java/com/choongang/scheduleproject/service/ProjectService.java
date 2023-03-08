9a1701cdccc5430ca64d76d8dd4c32446f275c3a
minjeong <jennygh2@hanmail.net>
2023-03-08 11:39:54 +0900

    projectVO ex..

---
+++ src/main/java/com/choongang/scheduleproject/service/ProjectService.java
@@ -0,0 +1,8 @@
+package com.choongang.scheduleproject.service;
+
+import com.choongang.scheduleproject.command.ProjectVO;
+
+public interface ProjectService {
+	
+	public int regist(ProjectVO vo);
+}
