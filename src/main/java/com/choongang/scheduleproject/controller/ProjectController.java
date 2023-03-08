8a8d87abbcf0af9a48ca1bca7b7407ac0ff0067a
minjeong <jennygh2@hanmail.net>
2023-03-08 11:36:19 +0900

    minjeong project add plus

--- src/main/java/com/choongang/scheduleproject/controller/ProjectController.java
+++ src/main/java/com/choongang/scheduleproject/controller/ProjectController.java
@@ -1,13 +1,26 @@
 package com.choongang.scheduleproject.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.choongang.scheduleproject.command.ProjectVO;
+import com.choongang.scheduleproject.service.ProjectService;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+	
+	@Autowired
+	@Qualifier("projectService")
+	private ProjectService projectService;
 
 	//프로젝트 생성
 	@GetMapping("/projectAdd")
@@ -40,6 +53,17 @@ public class ProjectController {
 	@GetMapping("/projectCalendar")
 	public String projectCalendar() {
 		return "/project/projectCalendar";
+	}
+	
+	//등록 요청
+	@PostMapping("/registForm")
+	public String registForm(ProjectVO vo, RedirectAttributes ra) {
+		int result = projectService.regist(vo);
+		String msg = result == 1 ? "정상 입력 되었습니다." : "등록에 실패하였습니다";
+		ra.addFlashAttribute("msg", msg);
+		System.out.println(result);
+		System.out.println(vo.toString());
+		return "redirect:/project/projectStarted"; 
 	}
 	
 }
