4702583e8b47a4cfbd58ed9040b8d9f6273b7b2c
Semin Lee <leesemeen@naver.com>
2023-03-13 11:35:35 +0900

    semin 회원가입, 로그인 (미완)

---
+++ src/main/java/com/choongang/scheduleproject/command/UserVO.java
@@ -0,0 +1,57 @@
+package com.choongang.scheduleproject.command;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserVO {
+	/*
+	 * @NotNull - null값만 허용하지 않음 (wrapper의 Integer, Long, String 등등)
+	 * @NotBlank - null값과 공백허용하지 않음 (String에만 적용)
+	 * @NotEmpty - null값을 허용하지 않음 (Array, list 적용)
+	 * @Pattern - 정규표현식에 맞는 문자열을 정의할 수 있음 (String에만 적용)
+	 * 
+	 * @Email - 이메일형식검증 (공백은 통과)
+	 * @Min - 최소값
+	 * @Max - 최대값
+	 */
+	@NotNull // 처음엔 모든 데이터에 NotNull 처리를 해줬는데, 로그인 시 에러가 발생하여 ID와 PW에만 NotNull을 걸어뒀습니다.
+	@Pattern(regexp = "^[a-zA-Z0-9]{4,12}$")
+	private String user_id;
+	
+	private int department_id;
+	
+	@Email
+	private String user_email;
+	
+	@Pattern(regexp = "[가-힣]{2,4}")
+	private String user_name;
+	
+	@NotNull
+	@Pattern(regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$")
+	private String user_pw;
+	
+	@Pattern(regexp = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$")
+	private String user_cell;
+	
+	private String user_birth;
+	private int user_active;
+	private String user_position;
+	private int user_role;
+	private int user_employeenumber;
+	private int user_loginfail;
+	private LocalDateTime user_regdate;
+	private String user_img;
+}
---
+++ src/main/java/com/choongang/scheduleproject/mail/service/MailService.java
@@ -0,0 +1,120 @@
+package com.choongang.scheduleproject.mail.service;
+
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.choongang.scheduleproject.command.EmailVO;
+import com.choongang.scheduleproject.user.service.UserMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@PropertySource("classpath:application.properties")
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MailService {
+	private final JavaMailSender javaMailSender;
+	private final UserMapper userMapper;
+	
+	//인증코드 - 생성은 메일을 보낼 때 createKey()를 이용해 ePw에 담아줄 것
+    private String ePw;
+    @Value("${spring.mail.username}")
+    private String id;
+
+    public MimeMessage createMessage(String to)throws MessagingException, UnsupportedEncodingException {
+        log.info("보내는 대상 : "+ to);
+        log.info("인증 번호 : " + ePw);
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(MimeMessage.RecipientType.TO, to); // to 보내는 대상
+        message.setSubject("폼미소프트 인증 코드"); //메일 제목
+
+        // 메일 내용 메일의 subtype을 html로 지정하여 html문법 사용 가능
+        String msg="";
+        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 인증코드</h1>";
+        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 인증코드를 3분 안에 입력 후 인증하기 버튼을 눌러주세요.</p>";
+        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
+        msg += ePw;
+        msg += "</td></tr></tbody></table></div>";
+
+        message.setText(msg, "utf-8", "html"); //내용, charset타입, subtype
+        message.setFrom(new InternetAddress(id,"폼미소프트_관리자")); //보내는 사람의 메일 주소, 보내는 사람 이름
+
+        return message;
+    }
+
+    // 인증코드 만들기
+    public static String createKey() {
+    	
+    	//랜덤 인증코드 생성
+        Random random = new Random(); 
+        int length = 6;
+
+        StringBuffer key = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int choice = random.nextInt(3);
+            switch(choice) {
+                case 0:
+                    key.append((char)((int)random.nextInt(25)+97));
+                    break;
+                case 1:
+                    key.append((char)((int)random.nextInt(25)+65));
+                    break;
+                case 2:
+                    key.append((char)((int)random.nextInt(10)+48));
+                    break;
+                default:
+                    break;
+            }
+        }
+    	
+        return key.toString();
+    }
+
+    /*
+        메일 발송
+        sendSimpleMessage의 매개변수로 들어온 to는 인증번호를 받을 메일주소
+        MimeMessage 객체 안에 내가 전송할 메일의 내용을 담아준다.
+        bean으로 등록해둔 javaMailSender 객체를 사용하여 이메일 send
+     */
+    public String sendSimpleMessage(String to, String joinReset) throws Exception { // 이메일 보내기
+    	
+    	ePw = createKey(); //메일을 보낼 때 랜덤문자열을 생성하여 보낼 때마다 다른 값을 넘기도록 설정
+    	
+        MimeMessage message = createMessage(to);
+        try {
+            EmailVO vo = new EmailVO();
+            LocalDateTime expireTime = LocalDateTime.now().plusMinutes(3); //시간제한 3분 
+            vo.setEmail(to);
+            vo.setCode(ePw);
+            vo.setExpire_time(expireTime);
+            vo.setJoinReset(joinReset); // 회원가입인지 비밀번호 초기화인지 구별하기 위함
+            int result = userMapper.sendVerifyCode(vo); //DB에 저장
+            if(result == 0) { //db에 저장 실패
+    			//RedirectAttributes를 여기서 사용할 수 없어서 userEmailError로 이동 후 처리
+    			return "redirect:/user/userEmailError";
+            }
+            
+            javaMailSender.send(message); // 메일 발송
+        } catch (MailException es) {
+            es.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw; // 메일로 보냈던 인증 코드를 서버로 리턴
+    }
+
+
+}
