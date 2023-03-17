fe9d5a8d9c3b6ae8ecf35f1c87f83d2002af4462
권도은 <kounduk9119t@naver.com>
2023-03-17 12:06:56 +0900

    doeun commit

--- src/main/java/com/choongang/scheduleproject/controller/UserBoardController.java
+++ src/main/java/com/choongang/scheduleproject/controller/UserBoardController.java
@@ -1,8 +1,5 @@
 package com.choongang.scheduleproject.controller;
 
-import java.util.ArrayList;
-
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,22 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.choongang.scheduleproject.board.service.AdminNoticeService;
+import com.choongang.scheduleproject.board.service.UserBoardService;
 import com.choongang.scheduleproject.command.AdminNoticeListVO;
 import com.choongang.scheduleproject.command.ProjectVO;
 import com.choongang.scheduleproject.project.service.ProjectService;
-import com.choongang.scheduleproject.service.AdminNoticeService;
-import com.choongang.scheduleproject.service.UserBoardService;
 import com.choongang.scheduleproject.util.Criteria;
 import com.choongang.scheduleproject.util.PageVO;
 
 @Controller
 @RequestMapping("/userboards")
 public class UserBoardController {
-	
+
 	@Autowired
 	@Qualifier("adminNoticeService")
 	private AdminNoticeService adminNoticeService;
-	
+
 	@Autowired
 	@Qualifier("userBoardService")
 	private UserBoardService userBoardService;
@@ -35,7 +32,7 @@ public class UserBoardController {
 	@Autowired
 	@Qualifier("projectService")
 	private ProjectService projectService;
-	
+
 	@GetMapping("/teamBoardList")
 	public String teamBoardList(Criteria cri, Model model,@RequestParam("pj_num") int pj_num) {
 		ProjectVO pjVO = projectService.getProject(pj_num);
@@ -43,7 +40,7 @@ public class UserBoardController {
 		model.addAttribute("pjVO",pjVO);
 		return "/userboards/teamBoardList";
 	}
-	
+
 	@GetMapping("/teamBoardRegist")
 	public String teamBoardRegist() {
 		return "/userboards/teamBoardRegist";
@@ -56,40 +53,33 @@ public class UserBoardController {
 	public String teamBoardContent() {
 		return "/userboards/teamBoardContent";
 	}
-	
-	@GetMapping("/noticeTableList")
+
+	@GetMapping("/notice-list")
 	public String noticeTableList(Criteria cri, Model model) {
-		
+
 		int total = adminNoticeService.getCount(cri);
 		model.addAttribute("AdminNoticeList", adminNoticeService.getList(cri)); //페이지에 넘길 데이터
-		
+
 		PageVO pageVO = new PageVO(cri, total); //페이징에 사용
 
 		model.addAttribute("pageVO", pageVO);
-		
-		
-		return "/userboards/noticeTableList";
+
+
+		return "/userboards/notice-list";
 	}
-	
+
 	//위의 noticeTableList를 상세 조회하는 컨트롤러
-	@GetMapping("/noticeContent")
+	@GetMapping("/notice-content")
 	public String noticeContent(@RequestParam("notice_num") int notice_num, Model model) {
-		
+
 		//클릭한 글 번호에 대한 내용을 조회
 		AdminNoticeListVO adminNoticeListVO = adminNoticeService.getContent(notice_num);
 		model.addAttribute("adminNoticeListVO", adminNoticeListVO);
-		
-		
-		return "/userboards/noticeContent";
+
+
+		return "/userboards/notice-content";
 	}
-	@GetMapping("/noticeRegist")
-	public String noticeRegist() {
-		return "/userboards/noticeRegist";
-	}
-	@GetMapping("/noticeModify")
-	public String noticeModify() {
-		return "/userboards/noticeModify";
-	}
-	
-	
+
+
+
 }
--- src/main/java/com/choongang/scheduleproject/util/PageVO.java
+++ src/main/java/com/choongang/scheduleproject/util/PageVO.java
@@ -11,17 +11,17 @@ public class PageVO {
 	private int start; //페이지네이션 시작번호
 	private boolean next; //다음버튼 활성화여부
 	private boolean prev; //이전버튼 활성화여부
-	
+
 	private int realEnd; //페이지네이션 실제끝번호
 	private int page; //사용자가 조회하는 페이지번호
 	private int amount; //화면에 1페이지에 나타나는 데이터개수
 	private int total; //전체게시글 수
-	
+
 	private Criteria cri; //페이지기준
 	private ArrayList<Integer> pageList; //화면에 반복시킬 페이지 번호
-	
-	private int pageCnt = 10; //페이지네이션 개수
-	
+
+	public static final int PAGE_COUNT = 10; //페이지네이션 개수
+
 	//생성자 - pageVO가 만들어질 때 cri, total을 받는다.
 	public PageVO(Criteria cri, int total) {
 		//계산에 필요한 값(페이지번호, 데이터개수, 전체게시글수, cri)를 초기화
@@ -29,41 +29,41 @@ public class PageVO {
 		this.amount = cri.getAmount();
 		this.total = total;
 		this.cri = cri;
-		
+
 		//끝페이지 계산
-		this.end = (int)Math.ceil(this.page / (double)pageCnt) * pageCnt;
-		
+		this.end = (int)Math.ceil(this.page / (double)PAGE_COUNT) * PAGE_COUNT;
+
 		//시작페이지번호 계산
-		this.start = this.end - pageCnt + 1;
-		
+		this.start = this.end - PAGE_COUNT + 1;
+
 		//실제끝번호 계산
 		this.realEnd = (int)Math.ceil(this.total / (double)this.amount );
-		
+
 		//마지막 페이지번호를 계산
 		this.end = this.end > this.realEnd ? this.realEnd : this.end;
-		
+
 		//이전버튼
 		this.prev = this.start > 1;
-		
+
 		//다음버튼
 		this.next = this.realEnd > this.end;
-		
+
 		//화면에서 반복시킬 페이지데이터
 		this.pageList = new ArrayList<>();
 	      for(int i = this.start; i<= this.end; i++) {
 	         pageList.add(i);
 	      }
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 
 }
