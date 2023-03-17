695d2be4ce7d96ec81b825525b76109a88ccdd86
Semin Lee <leesemeen@naver.com>
2023-03-17 19:20:21 +0900

    로그인 가능

--- src/main/java/com/choongang/scheduleproject/command/UserVO.java
+++ src/main/java/com/choongang/scheduleproject/command/UserVO.java
@@ -31,54 +31,39 @@ public class UserVO {
 	 */
 	@NotNull // 처음엔 모든 데이터에 NotNull 처리를 해줬는데, 로그인 시 에러가 발생하여 ID와 PW에만 NotNull을 걸어뒀습니다.
 	@Pattern(regexp = "^[a-zA-Z0-9]{4,12}$")
-	@JsonProperty("user_id")
 	private String userId;
 	
-	@JsonProperty("department_id")
 	private int departmentId;
 	
 	@Email
-	@JsonProperty("user_email")
 	private String userEmail;
 	
 	@Pattern(regexp = "[가-힣]{2,4}")
-	@JsonProperty("user_name")
 	private String userName;
 	
 	@NotNull
 	@Pattern(regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$")
-	@JsonProperty("user_pw")
 	private String userPw;
 	
 	@Pattern(regexp = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$")
-	@JsonProperty("user_cell")
 	private String userCell;
 	
-	@JsonProperty("user_birth")
 	private String userBirth;
 	
-	@JsonProperty("user_active")
 	private int userActive;
 	
-	@JsonProperty("user_position")
 	private String userPosition;
 	
-	@JsonProperty("user_role")
 	private int userRole;
 	
-	@JsonProperty("user_employeenumber")
 	private int userEmployeenumber;
 	
-	@JsonProperty("user_loginfail")
 	private int userLoginfail;
 	
-	@JsonProperty("user_regdate")
 	private LocalDateTime userRegdate;
 	
-	@JsonProperty("user_img")
 	private String userImg;
 	
-	@JsonProperty("department_name")
 	private String departmentName;
 }
 
