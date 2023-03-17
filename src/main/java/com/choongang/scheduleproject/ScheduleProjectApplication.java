0b5223b99ab44d2a8cbe3f8f03822466a1d4dd55
Wan <dhks3602@naver.com>
2023-03-17 19:21:06 +0900

    수정

--- src/main/java/com/choongang/scheduleproject/ScheduleProjectApplication.java
+++ src/main/java/com/choongang/scheduleproject/ScheduleProjectApplication.java
@@ -1,10 +1,13 @@
 package com.choongang.scheduleproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//@MapperScan(basePackages = {"com.choongang.scheduleproject.board.service","com.choongang.scheduleproject.project.service"
+//								,"com.choongang.scheduleproject.user.service","com.choongang.scheduleproject.mail.service"})
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ScheduleProjectApplication {
