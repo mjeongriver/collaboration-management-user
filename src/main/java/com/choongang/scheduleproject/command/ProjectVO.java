f85a52d10fcf215efd2d1908c57123f134f28f98
Wan <dhks3602@naver.com>
2023-03-17 20:06:13 +0900

    수정

--- src/main/java/com/choongang/scheduleproject/command/ProjectVO.java
+++ src/main/java/com/choongang/scheduleproject/command/ProjectVO.java
@@ -20,39 +20,32 @@ public class ProjectVO {
 	@NotNull
 	@NotBlank
 	@NotEmpty
-	@JsonProperty("pj_startdate")
 	private String pjStartdate;
-	
+
 	@NotNull
 	@NotBlank
 	@NotEmpty
-	@JsonProperty("pj_enddate")
 	private String pjEnddate;
-	
+
 	@NotNull
-	@JsonProperty("pj_name")
 	private String pjName;
 
 	@NotNull
-	@JsonProperty("pj_description")
 	private String pjDescription;
-	
-	@JsonProperty("pj_num")
+
 	private int pjNum;
-	
+
 	private String userName;
 	private String userId;
 
-	@JsonProperty("pj_writer")
 	private String pjWriter;
 	private boolean pjActive;
 	private boolean pjBookmark;
 	private String departmentName;
 	private int departmentId;
 
-	@JsonProperty("is_observer")
 	private boolean isObserver;
-  
+
 	private int pjTotalmember;
 	private int pjObservercount;
 	private int pjMembercount;
